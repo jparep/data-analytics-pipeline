642a818c8b4010eda1a7560bdc3eb39fd338ebbe
jparep <parepson@gmail.com>
2025-01-27 22:16:55 -0700

    Optimized product query

--- scripts/producst.sql
+++ scripts/producst.sql
@@ -1,16 +1,24 @@
-CREATE TABLE top_products AS
+WITH ranked_products AS (
+    SELECT
+        p.category,
+        p.product_name,
+        SUM(s.total_price) AS revenue,
+        RANK() OVER (PARTITION BY p.category ORDER BY SUM(s.total_price) DESC) AS rank
+    FROM
+        clean_sales_data s
+    JOIN
+        products p
+    ON
+        s.product_id = p.product_id
+    GROUP BY
+        p.category, p.product_name
+)
 SELECT
-    p.category,
-    p.product_name,
-    SUM(s.total_price) AS revenue,
-    RANK() OVER (PARTITION BY p.category ORDER BY SUM(s.total_price) DESC) AS rank
+    category,
+    product_name,
+    revenue,
+    rank
 FROM
-    clean_sales_data s
-JOIN
-    products p
-ON
-    s.product_id = p.product_id
-GROUP BY
-    p.category, p.product_name
-HAVING
+    ranked_products
+WHERE
     rank <= 5;
